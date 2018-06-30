44c9c55a3a536029f5ce1b143d2f8301c0606c52
zhongzhenga <616028858@qq.com>
2018-06-30 11:26:14 +0800

    修改api机制；

---
+++ culture-server/Controllers/ServerController.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.WebHost;
+using System.Web.Routing;
+using System.Web.Script.Serialization;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace culture_server.Controllers
+{
+    public class SessionRouteHandler : HttpControllerHandler, IRequiresSessionState
+    {
+        public SessionRouteHandler(RouteData routeData)
+            : base(routeData)
+        {
+
+        }
+    }
+
+    public class SessionControllerRouteHandler : HttpControllerRouteHandler
+    {
+        protected override IHttpHandler GetHttpHandler(RequestContext requestContext)
+        {
+            return new SessionRouteHandler(requestContext.RouteData);
+        }
+    }
+
+    public class ServerController : ApiController
+    {
+        #region 管理员登录授权
+        /// <summary>  
+        /// 管理员登录授权  
+        /// </summary>  
+        /// <param name="user">用户实体</param> 
+        /// <returns></returns>  
+        [AcceptVerbs("OPTIONS", "POST")]
+        public HttpResponseMessage login(dynamic user)
+        {
+            if (user == null)
+            {
+                return new HttpResponseMessage
+                {
+                    Content = new StringContent("", System.Text.Encoding.UTF8, "application/json")
+                };
+            }
+            string userName = Convert.ToString(user.userName);
+            string userPwd = Convert.ToString(user.userPwd);
+            string strSql = @"select    id
+                                        from dbo.c_admin
+                                        where userName = '{0}' and userPwd = '{1}'";
+            strSql = string.Format(strSql, userName, userPwd);
+            DataTable dt_user = DBHelper.SqlHelper.GetDataTable(strSql);
+            var data = new object { };
+            if (dt_user.Rows.Count == 1)
+            {
+                FormsAuthenticationTicket token = new FormsAuthenticationTicket(0, userName, DateTime.Now,
+                            DateTime.Now.AddHours(5), true, string.Format("{0}&{1}", userName, userPwd),
+                            FormsAuthentication.FormsCookiePath);
+                //返回登录结果、用户信息、用户验证票据信息
+                var Token = FormsAuthentication.Encrypt(token);
+                //将身份信息保存在数据库中，验证当前请求是否是有效请求
+                string str_token = @"insert into dbo.c_token (userId, token, expireDate) values ('{0}', '{1}', '{2}')";
+                str_token = string.Format(str_token, dt_user.Rows[0]["id"], Token, DateTime.Now.AddHours(3));
+                DBHelper.SqlHelper.ExecuteSql(str_token);
+
+                data = new
+                {
+                    success = true,
+                    token = Token,
+                    userId = dt_user.Rows[0]["id"]
+                };
+            }
+            else
+            {
+                data = new
+                {
+                    success = false,
+                    backMsg = "身份验证失败，请重试！"
+                };
+            }
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+            string json = serializer.Serialize(data);
+            return new HttpResponseMessage
+            {
+                Content = new StringContent(json, System.Text.Encoding.UTF8, "application/json")
+            };
+        }
+        #endregion
+
+        #region 管理员退出登录，清空Token
+        /// <summary>  
+        /// 管理员退出登录，清空Token  
+        /// </summary>  
+        /// <param name="id">用户ID</param>  
+        /// <returns></returns>
+        [AcceptVerbs("OPTIONS", "POST")]
+        public HttpResponseMessage LoginOut(dynamic id)
+        {
+            if (id == null)
+            {
+                return new HttpResponseMessage
+                {
+                    Content = new StringContent("", System.Text.Encoding.UTF8, "application/json")
+                };
+            }
+            string userId = Convert.ToString(id.userId);
+            int flag = 0;
+            try
+            {
+                //清空数据库该用户票据数据  
+                string str_clear = @"delete dbo.ls_token where userId='{0}'";
+                str_clear = string.Format(str_clear, userId);
+                flag = DBHelper.SqlHelper.ExecuteSql(str_clear);
+            }
+            catch (Exception ex) { }
+            //返回信息
+            var data = new object { };
+            if (flag > 0)
+            {
+                data = new
+                {
+                    success = true
+                };
+            }
+            else
+            {
+                data = new
+                {
+                    success = false,
+                    backMsg = "安全退出失败，请重试！"
+                };
+            }
+
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+            string json = serializer.Serialize(data);
+            return new HttpResponseMessage
+            {
+                Content = new StringContent(json, System.Text.Encoding.UTF8, "application/json")
+            };
+        }
+        #endregion
+    }
+}
--- culture-server/Global.asax.cs
+++ culture-server/Global.asax.cs
@@ -21,7 +21,6 @@ namespace culture_server
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
