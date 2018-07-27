ccb72daae131972e5512ab289f59b3a9fda86c0b
钟正 <616028858@qq.com>
2018-07-27 15:36:53 +0800

    修改视频；

---
+++ culture-server/Controllers/VideoController.cs
@@ -0,0 +1,352 @@
+﻿using culture_server.Core;
+using culture_server.Models;
+using culture_server.Util;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Script.Serialization;
+
+namespace culture_server.Controllers
+{
+    public class VideoController : ApiController
+    {
+        #region 获取视频列表
+        /// <summary>  
+        /// 获取视频列表 
+        /// </summary>  
+        /// <param name="id">id</param>  
+        /// <returns></returns>
+        [AcceptVerbs("OPTIONS", "GET")]
+        public HttpResponseMessage queryList(int pageNumber, int pageSize, string conditionText)
+        {
+            DataTable dt = new BLL.handleVideo().queryList(pageNumber, pageSize, conditionText);
+            Object data;
+            if (dt.Rows.Count >= 0)
+            {
+                List<video> list = new List<video>();
+                for (int i = 0; i < dt.Rows.Count; i++)
+                {
+                    list.Add(generateVideo(dt.Rows[i]));
+                }
+
+                data = new
+                {
+                    success = true,
+                    backData = list
+                };
+            }
+            else
+            {
+                data = new
+                {
+                    success = false,
+                    backMsg = "数据异常"
+                };
+            }
+
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+            string json = serializer.Serialize(data);
+            return new HttpResponseMessage
+            {
+                Content = new StringContent(json, System.Text.Encoding.UTF8, "application/json")
+            };
+        }
+        #endregion
+
+        #region 获取管理视频列表
+        /// <summary>  
+        /// 获取管理视频列表 
+        /// </summary>  
+        /// <param name="id">id</param>  
+        /// <returns></returns>
+        [AcceptVerbs("OPTIONS", "GET")]
+        public HttpResponseMessage queryListByAdmin(dynamic d)
+        {
+            DataTable dt = new BLL.handleArt().queryListByAdmin();
+            Object data;
+            if (dt.Rows.Count >= 0)
+            {
+                List<video> list = new List<video>();
+                for (int i = 0; i < dt.Rows.Count; i++)
+                {
+                    list.Add(generateVideo(dt.Rows[i]));
+                }
+
+                data = new
+                {
+                    success = true,
+                    backData = list
+                };
+            }
+            else
+            {
+                data = new
+                {
+                    success = false,
+                    backMsg = "数据异常"
+                };
+            }
+
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+            string json = serializer.Serialize(data);
+            return new HttpResponseMessage
+            {
+                Content = new StringContent(json, System.Text.Encoding.UTF8, "application/json")
+            };
+        }
+        #endregion
+
+        #region 获取艺术信息详情
+        /// <summary>  
+        /// 获取艺术信息详情 
+        /// </summary>  
+        /// <param name="id">id</param>  
+        /// <returns></returns>
+        [AcceptVerbs("OPTIONS", "GET")]
+        public HttpResponseMessage queryDetail(string id)
+        {
+            DataTable dt = new BLL.handleArt().queryDetail(id);
+            Object data;
+            if (dt.Rows.Count == 1)
+            {
+                data = new
+                {
+                    success = true,
+                    backData = generateVideo(dt.Rows[0])
+                };
+            }
+            else
+            {
+                data = new
+                {
+                    success = false,
+                    backMsg = "数据异常"
+                };
+            }
+
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+            string json = serializer.Serialize(data);
+            return new HttpResponseMessage
+            {
+                Content = new StringContent(json, System.Text.Encoding.UTF8, "application/json")
+            };
+        }
+        #endregion
+
+        #region 更新或者新增艺术信息
+        /// <summary>  
+        /// 更新或者新增艺术信息 
+        /// </summary>  
+        /// <param name="id">id</param>  
+        /// <returns></returns>
+        [SupportFilter]
+        [AcceptVerbs("OPTIONS", "POST")]
+        public HttpResponseMessage save(dynamic d)
+        {
+            Object data;
+
+            try
+            {
+                BLL.handleArt culture = new BLL.handleArt();
+                bool flag = false;
+                flag = culture.saveAP(d);
+
+                if (flag)
+                {
+                    data = new
+                    {
+                        success = true
+                    };
+                }
+                else
+                {
+                    data = new
+                    {
+                        success = false,
+                        backMsg = "保存信息失败"
+
+                    };
+                }
+            }
+            catch (Exception ex)
+            {
+                data = new
+                {
+                    success = false,
+                    backMsg = "服务异常"
+
+                };
+            }
+
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+            string json = serializer.Serialize(data);
+            return new HttpResponseMessage
+            {
+                Content = new StringContent(json, System.Text.Encoding.UTF8, "application/json")
+            };
+        }
+        #endregion
+
+        #region 删除艺术
+        /// <summary>  
+        /// 删除艺术
+        /// </summary>  
+        /// <param name="id">id</param>  
+        /// <returns></returns>
+        [AcceptVerbs("OPTIONS", "POST")]
+        public HttpResponseMessage delete(dynamic d)
+        {
+            string id = d.id;
+            object data = new object();
+            try
+            {
+                BLL.handleArt culture = new BLL.handleArt();
+                bool flag = false;
+
+                flag = culture.delete(id);
+
+                if (flag)
+                {
+                    data = new
+                    {
+                        success = true
+                    };
+                }
+                else
+                {
+                    data = new
+                    {
+                        success = false,
+                        backMsg = "删除新闻信息失败"
+
+                    };
+                }
+            }
+            catch (Exception ex)
+            {
+                data = new
+                {
+                    success = false,
+                    backMsg = "服务异常"
+
+                };
+            }
+
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+            string json = serializer.Serialize(data);
+            return new HttpResponseMessage
+            {
+                Content = new StringContent(json, System.Text.Encoding.UTF8, "application/json")
+            };
+        }
+        #endregion
+
+        #region 审核艺术品
+        /// <summary>  
+        /// 审核艺术品 
+        /// </summary>  
+        /// <param name="id">id</param>  
+        /// <returns></returns>
+        [SupportFilter]
+        [AcceptVerbs("OPTIONS", "POST")]
+        public HttpResponseMessage review(dynamic d)
+        {
+            Object data;
+
+            try
+            {
+                BLL.handleArt art = new BLL.handleArt();
+                int flag = art.review(d);
+
+                if (flag == 1)
+                {
+                    data = new
+                    {
+                        success = true
+                    };
+                }
+                else if (flag == 2)
+                {
+                    data = new
+                    {
+                        success = false,
+                        backMsg = "审核失败"
+
+                    };
+                }
+                else if (flag == -1)
+                {
+                    data = new
+                    {
+                        success = false,
+                        backMsg = "不允许审核"
+
+                    };
+                }
+                else if (flag == 0)
+                {
+                    data = new
+                    {
+                        success = false,
+                        backMsg = "艺术品不存在"
+
+                    };
+                }
+                else
+                {
+                    data = new
+                    {
+                        success = false,
+                        backMsg = "系统异常，请重试！"
+
+                    };
+                }
+            }
+            catch (Exception ex)
+            {
+                data = new
+                {
+                    success = false,
+                    backMsg = "服务异常"
+
+                };
+            }
+
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+            string json = serializer.Serialize(data);
+            return new HttpResponseMessage
+            {
+                Content = new StringContent(json, System.Text.Encoding.UTF8, "application/json")
+            };
+        }
+        #endregion      
+
+        #region 私有方法集
+        //返回video对象
+        private video generateVideo(dynamic d)
+        {
+            video n = new video();
+            n.id = d["id"].ToString();
+            n.videoFile = util.generateListImage(d["fileId"].ToString());  
+            n.videoTitle = d["videoTitle"].ToString();
+            n.videoSubTitle = d["videoSubTitle"].ToString();
+            n.videoCover = util.generateListImage(d["videoCover"].ToString());
+            n.videoAuthor = d["videoAuthor"].ToString();
+            n.videoBrief = d["videoBrief"].ToString();                             
+            n.state = Convert.ToInt32(d["state"].ToString());
+            n.updator = d["updator"].ToString();
+            n.updatorName = d["updatorName"].ToString();
+            n.update_time = d["update_time"].ToString();
+            n.creator = d["creator"].ToString();
+            n.creatorName = d["creatorName"].ToString();
+            n.create_time = d["create_time"].ToString();
+
+            return n;
+        }
+        #endregion
+    }
+}
---
+++ culture-server/Models/video.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace culture_server.Models
+{
+    public class video
+    {
+        public string id { set; get; }
+        public image videoFile { set; get; }
+        public string videoTitle { set; get; }
+        public string videoSubTitle { set; get; }
+        public image videoCover { set; get; }
+        public string videoAuthor { set; get; }
+        public string videoBrief { set; get; }
+        public int state { set; get; }
+        public string updator { set; get; }
+        public string updatorName { set; get; }
+        public string update_time { set; get; }
+        public string creator { set; get; }
+        public string creatorName { set; get; }
+        public string typeName { set; get; }
+        public string create_time { set; get; }
+    }
+}
