a50014ed22b7dc920a539bc0e1fce4ed813f47ea
钟正 <616028858@qq.com>
2019-01-21 17:33:58 +0800

    修改user模型；

--- culture-server/Models/user.cs
+++ culture-server/Models/user.cs
@@ -8,11 +8,14 @@ namespace culture_server.Models
     public class user
     {
         public string id { set; get; }
-        public image avatar { set; get; }
+        public string openid { set; get; }
+        public string nickname { set; get; }
         public string sex { set; get; }
-        public string userName { set; get; }
-        public string nickName { set; get; }
-        public string telephone { set; get; }  
+        public string province { set; get; }
+        public string city { set; get; }
+        public string country { set; get; }
+        public string headimgurl { set; get; }
+        public string unionid { set; get; }
         public int state { set; get; }
         public int isDelete { set; get; }
         public string create_time { set; get; }
